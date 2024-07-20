294397871170fe52f5065664d3ebf62faebd70d5
Tereta Alexander <atereta@gomage.com>
2024-07-20 16:56:20 +0300

    Fix dev tools and versions

--- dev/repositories/push.sh
+++ dev/repositories/push.sh
@@ -10,6 +10,10 @@ mkdir -p "$ROOTDIR/dev/repositories"
 mkdir -p "$ROOTDIR/dev/repositories/git"
 
 for REPO in "${REPOSITORIES[@]}"; do
+  echo "-----$REPO-----"
+
+  echo "cd \"$ROOTDIR/vendor/tereta/$REPO\"; git config pull.rebase false" | bash
+  echo "cd \"$ROOTDIR/vendor/tereta/$REPO\"; git pull" | bash
   echo "cd \"$ROOTDIR/vendor/tereta/$REPO\"; git commit -am \"Release: $VERSION\"; git push" | bash
   echo "cd \"$ROOTDIR/vendor/tereta/$REPO\"; git tag $VERSION; git push --tags" | bash
 done
