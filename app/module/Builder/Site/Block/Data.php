37c5cba779bbfea4233af38ca10ff98ff6c5977e
Tereta Alexander <atereta@gomage.com>
2024-08-30 16:06:12 +0300

    Bug fixing and process social and site blocks

---
+++ app/module/Builder/Site/Block/Data.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Builder\Site\Block;
+
+use Framework\View\Php\Template;
+
+/**
+ * @class Builder\Site\Block\Data
+ */
+class Data extends Template
+{
+}
---
+++ app/module/Builder/Social/Block/Icons.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace Builder\Social\Block;
+
+use Framework\View\Php\Template;
+
+/**
+ * @class Builder\Social\Block\Icons
+ */
+class Icons extends Template
+{
+}
