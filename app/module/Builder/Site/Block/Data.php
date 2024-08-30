64046e4559757f47022017ac9144ffe580c05237
Tereta Alexander <atereta@gomage.com>
2024-08-31 00:25:14 +0300

    Progress

--- app/module/Builder/Site/Block/Data.php
+++ app/module/Builder/Site/Block/Data.php
@@ -2,11 +2,39 @@
 
 namespace Builder\Site\Block;
 
+use Builder\Site\Model\Site as SiteModel;
 use Framework\View\Php\Template;
+use Builder\Site\Model\Repository as SiteRepository;
+use Exception;
 
 /**
  * @class Builder\Site\Block\Data
  */
 class Data extends Template
 {
+    /**
+     * @var SiteModel $siteModel
+     */
+    private SiteModel $siteModel;
+
+    /**
+     * @return void
+     * @throws Exception
+     */
+    protected function construct(): void
+    {
+        $this->siteModel = SiteRepository::getInstance()->getByDomain($_SERVER['HTTP_HOST'] ?? new Exception('Domain not found'));
+        $this->assign('site', $this->siteModel->getData());
+    }
+
+    public function phoneNumber($test): string
+    {
+        return $test;
+    }
+
+    public function concat($prefix, $test): string
+    {
+        return $prefix . $test;
+    }
+
 }
