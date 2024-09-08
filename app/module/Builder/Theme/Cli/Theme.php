60506e53040a9ff60e452dacaf16373aaa34ba6e
Tereta Alexander <atereta@gomage.com>
2024-09-08 13:03:25 +0300

    Theme section

--- app/module/Builder/Theme/Cli/Theme.php
+++ app/module/Builder/Theme/Cli/Theme.php
@@ -5,10 +5,8 @@ namespace Builder\Theme\Cli;
 use Exception;
 use Framework\Application\Manager;
 use Framework\Cli\Interface\Controller;
-use Builder\Theme\Model\Theme as ThemeModel;
-use Builder\Theme\Model\Resource\Theme as ThemeResource;
-use Builder\Theme\Model\Resource\Theme\Collection as ThemeCollection;
-use DirectoryIterator;
+use Builder\Theme\Model\Index as ThemeIndex;
+use Builder\Theme\Model\Theme\Repository as ThemeRepository;
 use Framework\Cli\Symbol;
 
 /**
@@ -16,14 +14,17 @@ use Framework\Cli\Symbol;
  */
 class Theme implements Controller
 {
-    private ThemeResource $themeResource;
+    /**
+     * @var ThemeRepository $themeRepository
+     */
+    private ThemeRepository $themeRepository;
 
-    private ThemeCollection $themeCollection;
-
+    /**
+     * @throws Exception
+     */
     public function __construct()
     {
-        $this->themeResource = new ThemeResource;
-        $this->themeCollection = new ThemeCollection;
+        $this->themeRepository = new ThemeRepository;
     }
 
     /**
@@ -35,73 +36,13 @@ class Theme implements Controller
     public function registerThemes(): void
     {
         $viewDirectory = Manager::getInstance()->getConfig()->get('viewDirectory');
-        $directoryIterator = new DirectoryIterator($viewDirectory);
-        foreach ($directoryIterator as $item) {
-            if($item->isDot()) continue;
-            if (str_starts_with($item->getFilename(), '.')) continue;
-
-            $this->registerTheme($viewDirectory, $item);
+        list($registered, $unregistered) = $this->themeRepository->reindex($viewDirectory);
+        foreach ($registered as $item) {
+            echo Symbol::COLOR_GREEN . "Registered: {$item}\n" . Symbol::COLOR_RESET;
         }
 
-        foreach ($this->themeCollection as $item) {
-            $item->get('identifier');
-            $this->unregisterTheme($viewDirectory, $item);
+        foreach ($unregistered as $item) {
+            echo Symbol::COLOR_RED . "Unregistered: {$item}\n" . Symbol::COLOR_RESET;
         }
-
-        echo PHP_EOL;
-    }
-
-    private function unregisterTheme(string $viewDirectory, ThemeModel $themeModel): void
-    {
-        $themeDirectory = "{$viewDirectory}/{$themeModel->get('identifier')}";
-        $descriptionFile = "{$themeDirectory}/view.json";
-
-        $description = [];
-        if (is_file($descriptionFile)) {
-            $description = json_decode(file_get_contents($descriptionFile), true);
-        }
-
-        if ($this->isValidDescription($description)) {
-            return;
-        }
-
-        $this->themeResource->delete($themeModel);
-        echo Symbol::COLOR_RED . "DELETED: {$themeModel->get('identifier')}\n" . Symbol::COLOR_RESET;
-    }
-
-    private function isValidDescription(array $description): bool
-    {
-        $allowed = isset($description['name']) && $description['name'];
-        $allowed = $allowed && isset($description['description']) && $description['description'];
-        $allowed = $allowed && isset($description['imageTable']) && $description['imageTable'];
-        $allowed = $allowed && isset($description['imageMobile']) && $description['imageMobile'];
-        if (!$allowed) return false;
-        return true;
-    }
-
-    private function registerTheme(string $viewDirectory, DirectoryIterator $item): void
-    {
-        $themeDirectory = "{$viewDirectory}/{$item->getFilename()}";
-        $descriptionFile = "{$themeDirectory}/view.json";
-
-        $description = [];
-        if (is_file($descriptionFile)) {
-            $description = json_decode(file_get_contents($descriptionFile), true);
-        }
-
-        if (!$this->isValidDescription($description)) {
-            return;
-        }
-
-        $this->themeResource->load($themeModel = new ThemeModel, ['identifier' => $item]);
-        if ($themeModel->get('id')) {
-            return;
-        }
-
-        $themeModel->set('identifier', $item->getFilename());
-
-        $this->themeResource->save($themeModel);
-
-        echo Symbol::COLOR_GREEN . "CREATED: {$description['name']} [{$item->getFilename()}]\n" . Symbol::COLOR_RESET;
     }
 }
